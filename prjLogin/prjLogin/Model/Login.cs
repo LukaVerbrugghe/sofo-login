beddfff489d349a160ec9faafe0ef035c5ce0640
Luka Verbrugghe <135641479+LukaVerbrugghe@users.noreply.github.com>
2025-01-16 11:46:47 +0100

    Stap 2: model

---
+++ prjLogin/prjLogin/Model/Login.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjLogin.Model
+{
+    //dit bestand heeft altijd de naam van je database
+    //stap 1: internal vervangen met public
+    public class Login
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
